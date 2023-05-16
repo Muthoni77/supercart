36a00b5110e79c095826dcad3d30a7968b2b2088
jonathan_baraza <jbaraza46@gmail.com>
2023-05-16 13:31:07 +0300

    entire page loading

--- components/screenLoaders/PageLoading.tsx
+++ components/screenLoaders/PageLoading.tsx
@@ -1,15 +1,17 @@
-import Image from "next/image";
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 function PageLoading() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center">
-      <img
-        className="w-1/3 md:w-1/6 animate__animated animate__pulse animate__slower animate__infinite"
-        src={"./logo.png"}
-      />
-      <img className="" src={"./loading2.gif"} />
-    </div>
+    <>
+      <div className="w-screen h-screen flex flex-col items-center justify-center">
+        <img
+          className="w-1/3 md:w-1/6 animate__animated animate__pulse animate__slower animate__infinite"
+          src={"./logo.png"}
+        />
+        <img className="" src={"./loading2.gif"} />
+      </div>
+    </>
   );
 }
 
