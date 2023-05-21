19f5f9cc8ee5cead91374cbde894c26cfc51d1e7
jonathan_baraza <jbaraza46@gmail.com>
2023-05-21 15:31:03 +0300

    fix: rename

---
+++ components/Cart/CartPreview.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartPreview = () => {
+  return (
+    <div className="w-1/4 bg-white fixed top-16 p-6 rounded-3xl  right-12 shadow z-[50]">
+      My cart items
+    </div>
+  );
+};
+
+export default CartPreview;
