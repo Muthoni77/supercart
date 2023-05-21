cc093dd85d9f7b7b799f94dd07864f145d8f00b9
jonathan_baraza <jbaraza46@gmail.com>
2023-05-21 16:15:56 +0300

    sub totals fix

--- components/Cart/CartPreview.tsx
+++ components/Cart/CartPreview.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import CartItem from "./CartItem";
+import PriceDetails from "./priceDetails";
 
 const CartPreview = () => {
   return (
     <div className="w-1/3 bg-white fixed top-[65px] p-6 rounded-3xl  right-12 shadow z-[50]">
-      <div className="w-full">
-        <span className="text-lg font-bold text-[#334155]">Shopping Cart</span>
+      <div className="w-full flex flex-col space-y-3">
+        <span
+          className="text-lg font-bold text-[#334155] "
+          style={{ marginBottom: "10px" }}
+        >
+          Shopping Cart
+        </span>
         <CartItem />
       </div>
+      <PriceDetails />
     </div>
   );
 };
