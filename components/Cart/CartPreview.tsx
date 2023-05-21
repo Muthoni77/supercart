6cd351d79bf750435a03cac368bc10546d2a6bc2
jonathan_baraza <jbaraza46@gmail.com>
2023-05-21 16:30:04 +0300

    subtotals

--- components/Cart/CartPreview.tsx
+++ components/Cart/CartPreview.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import CartItem from "./CartItem";
-import PriceDetails from "./priceDetails";
+import PriceDetails from "./PriceDetails";
 
 const CartPreview = () => {
   return (
-    <div className="w-1/3 bg-white fixed top-[65px] p-6 rounded-3xl  right-12 shadow z-[50]">
-      <div className="w-full flex flex-col space-y-3">
+    <div className="w-1/3 bg-white fixed top-[65px]  rounded-3xl  right-12 shadow z-[50]">
+      <div className="w-full flex flex-col space-y-3 p-6">
         <span
           className="text-lg font-bold text-[#334155] "
           style={{ marginBottom: "10px" }}
