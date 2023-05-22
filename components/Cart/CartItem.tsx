dac57dfdd3d617f590d3a36b928a5d7327a1cd29
jonathan_baraza <jbaraza46@gmail.com>
2023-05-22 21:40:57 +0300

    cart Item data

--- components/Cart/CartItem.tsx
+++ components/Cart/CartItem.tsx
@@ -1,22 +1,29 @@
+import { CartItem } from "@/Types/products";
 import React from "react";
 
-const CartItem = () => {
+interface PropTypes {
+  product: CartItem;
+}
+
+const CartItem = ({ product }: PropTypes) => {
   return (
     <div className={`flex mt-3 border-b border-gray-100 py-4`}>
       <div className="bg-[#f1f5f9] rounded-xl flex items-center justify-center w-1/5">
-        <img src={"./products/1.webp"} className="w-full" />
+        <img src={product.photo} className="w-full" />
       </div>
       <div className="w-4/5 flex justify-between px-4">
         <div className="flex flex-col justify-between space-y-4">
           <div className="flex flex-col space-y-1">
-            <span className="font-bold text-[#334155]">Rey Nylon Backpack</span>
+            <span className="font-bold text-[#334155]">{product.title}</span>
             <span className="flex items-center text-gray-500 text-sm">
-              Natural{" "}
-              <span className="border-l border-gray-300 ml-2 pl-2">XL</span>
+              {product.color}
+              <span className="border-l border-gray-300 ml-2 pl-2">
+                {product.size}
+              </span>
             </span>
           </div>
           <span className="flex items-center text-gray-500 text-sm mt-3">
-            Qty <span className="ml-1">1</span>
+            Qty <span className="ml-1">{product.quantity}</span>
           </span>
         </div>
         <div className="flex flex-col justify-between">
