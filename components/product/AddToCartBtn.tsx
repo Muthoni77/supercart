f0f95b25b6a8dc4e44cc8c9a57b00c022cf23968
jonathan_baraza <jbaraza46@gmail.com>
2023-05-22 21:22:21 +0300

    feat: pass quantity to parent

--- components/product/AddToCartBtn.tsx
+++ components/product/AddToCartBtn.tsx
@@ -2,8 +2,9 @@ import { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { HiOutlineShoppingBag } from "react-icons/hi";
 
-const AddToCartBtn = () => {
-  const [num, setNum] = useState(1);
+const AddToCartBtn = ({ addToCart, setQuantity }: any) => {
+  const [num, setNum] = useState<number>(1);
+
   const handleSetNum = (isAdding: boolean) => {
     switch (isAdding) {
       case true:
@@ -16,13 +17,14 @@ const AddToCartBtn = () => {
         break;
     }
   };
+
   return (
     <div className="pt-2 pb-7 flex items-center justify-between border-b-1 border-b-gray-200 border-b">
       <div className="flex items-center justify-between w-1/3 bg-[#f5f8fb] p-3 rounded-3xl">
         <div
-          className={`border rounded-full p-2 border-1 cursor-pointer hover:border-gray-700 border-[${
-            num > 1 ? "#9ca3af" : "#c8cdd5"
-          }]`}
+          className={`border rounded-full p-2 border-1 ${
+            num > 1 && "cursor-pointer hover:border-gray-700"
+          } border-[${num > 1 ? "#9ca3af" : "#c8cdd5"}]`}
           onClick={() => {
             handleSetNum(false);
           }}
@@ -40,7 +42,12 @@ const AddToCartBtn = () => {
         </div>
       </div>
       <div className="w-2/3 pl-4 ">
-        <div className="flex items-center justify-center rounded-3xl p-4  font-bold w-full bg-[#0f172a] text-white hover:cursor-pointer hoverDownEffect shadow-xl">
+        <div
+          onClick={() => {
+            addToCart(num);
+          }}
+          className="flex items-center justify-center rounded-3xl p-4  font-bold w-full bg-[#0f172a] text-white hover:cursor-pointer hoverDownEffect shadow-xl"
+        >
           <HiOutlineShoppingBag size={20} className="mr-2" /> Add to cart
         </div>
       </div>
