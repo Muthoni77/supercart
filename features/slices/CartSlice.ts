1be15eaa956eae530358a84ec890f875547cf8fc
jonathan_baraza <jbaraza46@gmail.com>
2023-05-25 21:53:59 +0300

    feat: slice action to calculate total

--- features/slices/CartSlice.ts
+++ features/slices/CartSlice.ts
@@ -1,9 +1,12 @@
 import { CartItem } from "@/Types/products";
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
+import { store } from "../store/store";
 
 const initialState: any = {
   products: [],
+  subtotal: 0,
+  loading: false,
 };
 
 const CartSlice = createSlice({
@@ -13,12 +16,22 @@ const CartSlice = createSlice({
     addNewProduct: (state: any, action: any) => {
       state.products = [action.payload, ...state.products];
       toast.success("Added to cart successfully");
+      CartSlice.caseReducers.calculateTotal(state, action);
+    },
+    calculateTotal: (state, action: any) => {
+      let total = 0.0;
+      state.products.forEach((product: any) => {
+        let productPrice = product.quantity * product.price;
+        total += productPrice;
+      });
+      state.subtotal = total.toFixed(2);
     },
     addProductQuantity: (state, action) => {},
     removeProduct: (state, action) => {
       state.products = state.products.filter(
         (product: any) => product.title! !== action.payload
       );
+      CartSlice.caseReducers.calculateTotal(state, action);
     },
   },
 });
