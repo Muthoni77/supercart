6cf7bd8c6f97da0d5030cb12bcf67052916026e3
jonathan_baraza <jbaraza46@gmail.com>
2023-05-27 22:49:19 +0300

    cart page layout

---
+++ app/cart/layout.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const CartLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <Navbar />
+      <div className="w-full min-h-[70vh]">{children}</div>
+      <Footer />
+    </>
+  );
+};
+
+export default CartLayout;
---
+++ app/cart/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Cart = () => {
+  return (
+    <div className="p-4">
+      <span className="font-bold text-4xl bg-red-500 h-[50vh]">
+        Shopping Cart
+      </span>
+    </div>
+  );
+};
+
+export default Cart;
