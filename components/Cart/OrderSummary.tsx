4f75e0f8e5fe4ed405666af47beb44b4950488a0
jonathan_baraza <jbaraza46@gmail.com>
2023-05-28 12:51:01 +0300

    shipping and taxes feat design

--- components/Cart/OrderSummary.tsx
+++ components/Cart/OrderSummary.tsx
@@ -1,29 +1,33 @@
-import React from "react";
+import { useState } from "react";
 import { useAppSelector } from "@/hooks";
 import { useRouter } from "next/navigation";
 
 const OrderSummary = () => {
   const { subtotal } = useAppSelector((state) => state.cart);
   const router = useRouter();
+  const [shipping, setShipping] = useState<number>(15.0);
+  const [tax, setTax] = useState<number>(4.0);
   return (
-    <div className="bg-[#f9fafb] w-full px-6 pb-4 pt-6 rounded-bl-3xl rounded-br-3xl z-50">
-      <div>
-        <span className="font-bold">Order Summary</span>
-        <div className="flex  items-center justify-between">
-          <span>Subtotal</span>
-          <span>${subtotal}</span>
+    <div className="w-full px-6 pb-4 pt-6 rounded-bl-3xl rounded-br-3xl z-50">
+      <div className="flex flex-col space-y-8">
+        <span className="font-bold text-base">Order Summary</span>
+        <div className="flex  items-center justify-between border-b pb-3 text-sm">
+          <span className="text-gray-500">Subtotal</span>
+          <span className="font-semibold">${subtotal}</span>
         </div>
-        <div className="flex  items-center justify-between">
-          <span>Shipping estimate</span>
-          <span>${subtotal}</span>
+        <div className="flex  items-center justify-between border-b text-sm pb-3">
+          <span className="text-gray-500">Shipping estimate</span>
+          <span className="font-semibold">${shipping.toFixed(2)}</span>
         </div>
-        <div className="flex  items-center justify-between">
-          <span>Tax estimate</span>
-          <span>${subtotal}</span>
+        <div className="flex  items-center justify-between border-b text-sm pb-3">
+          <span className="text-gray-500">Tax estimate</span>
+          <span className="font-semibold">${tax.toFixed(2)}</span>
         </div>
-        <div className="flex  items-center justify-between font-bold">
+        <div className="flex  items-center justify-between border-b text-base pb-3 font-bold">
           <span>Order total</span>
-          <span>${subtotal}</span>
+          <span className="font-semi-bold">
+            ${(Number(subtotal) + tax + shipping).toFixed(2)}
+          </span>
         </div>
         <div className="flex items-center justify-between mt-6">
           <div className="w-3/4 mx-auto">
