998c4c906b536dc5e661da55ec9881ee957e9263
jonathan_baraza <jbaraza46@gmail.com>
2023-05-28 13:33:27 +0300

    add quantity buttons

--- components/Cart/CartItem.tsx
+++ components/Cart/CartItem.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import { CartItem } from "@/Types/products";
 import { removeProduct } from "@/features/slices/CartSlice";
 import { useAppDispatch } from "@/hooks";
-import React from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 interface PropTypes {
   product: CartItem;
@@ -11,6 +12,20 @@ const CartItem = ({ product }: PropTypes) => {
   const dispatch = useAppDispatch();
   const handleRemove = () => {
     dispatch(removeProduct(product.title));
+  };
+  const [num, setNum] = useState<number>(1);
+
+  const handleSetNum = (isAdding: boolean) => {
+    switch (isAdding) {
+      case true:
+        setNum(num + 1);
+        break;
+      case false:
+        num > 1 && setNum(num - 1);
+        break;
+      default:
+        break;
+    }
   };
   return (
     <div className={`flex mt-3 border-b border-gray-100 py-4`}>
@@ -31,6 +46,29 @@ const CartItem = ({ product }: PropTypes) => {
           <span className="flex items-center text-gray-500 text-sm mt-3">
             Qty <span className="ml-1">{product.quantity}</span>
           </span>
+        </div>
+        <div className="">
+          <div className="flex items-center space-x-5 justify-between p-3 rounded-3xl">
+            <div
+              className={`border rounded-full p-2 border-1 ${
+                num > 1 && "cursor-pointer hover:border-gray-700"
+              } border-[${num > 1 ? "#9ca3af" : "#c8cdd5"}]`}
+              onClick={() => {
+                handleSetNum(false);
+              }}
+            >
+              <AiOutlineMinus color={`${num > 1 ? "black" : "#838891"}`} />
+            </div>
+            <span className="">{num}</span>
+            <div
+              className={`border  rounded-full p-2 border-1 border-[#9ca3af] cursor-pointer hover:border-gray-700`}
+              onClick={() => {
+                handleSetNum(true);
+              }}
+            >
+              <AiOutlinePlus />
+            </div>
+          </div>
         </div>
         <div className="flex flex-col justify-between">
           <span className="text-[#26c661] border-[#26c661] border-2 rounded-lg text-sm w-[60px] font-bold p-[2px] flex items-center justify-center">
