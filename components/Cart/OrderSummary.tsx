d513827a5f2f361c2ce7de77cd2ea8404db188d0
jonathan_baraza <jbaraza46@gmail.com>
2023-05-28 13:33:51 +0300

    padding fix

--- components/Cart/OrderSummary.tsx
+++ components/Cart/OrderSummary.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useAppSelector } from "@/hooks";
 import { useRouter } from "next/navigation";
+import { BiInfoCircle } from "react-icons/bi";
 
 const OrderSummary = () => {
   const { subtotal } = useAppSelector((state) => state.cart);
@@ -9,8 +10,8 @@ const OrderSummary = () => {
   const [tax, setTax] = useState<number>(4.0);
   return (
     <div className="w-full px-6 pb-4 pt-6 rounded-bl-3xl rounded-br-3xl z-50">
-      <div className="flex flex-col space-y-8">
-        <span className="font-bold text-base">Order Summary</span>
+      <div className="flex flex-col space-y-6">
+        <span className="font-bold text-base mb-3">Order Summary</span>
         <div className="flex  items-center justify-between border-b pb-3 text-sm">
           <span className="text-gray-500">Subtotal</span>
           <span className="font-semibold">${subtotal}</span>
@@ -30,10 +31,25 @@ const OrderSummary = () => {
           </span>
         </div>
         <div className="flex items-center justify-between mt-6">
-          <div className="w-3/4 mx-auto">
-            <button className="w-full p-[14px] border hover:cursor-pointer bg-[#0f172a] text-white hoverDownEffect  rounded-3xl">
+          <div className="w-full mx-auto">
+            <button className="w-full p-[14px] shadow border hover:cursor-pointer bg-[#0f172a] text-white hoverDownEffect  rounded-3xl">
               Checkout
             </button>
+            <span
+              style={{ fontWeight: 500 }}
+              className="flex items-center text-gray-500  text-sm mx-auto mt-4 justify-center"
+            >
+              <BiInfoCircle size={18} className="mr-1 " />
+              Learn more{" "}
+              <span className="text-gray-700 underline mx-1 hover:cursor-pointer">
+                Taxes
+              </span>
+              and
+              <span className="text-gray-700 underline mx-1 hover:cursor-pointer">
+                Shipping
+              </span>
+              information
+            </span>
           </div>
         </div>
       </div>
