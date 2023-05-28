33d9c8df0cd08c108a390f296291fb5e8b02c8af
jonathan_baraza <jbaraza46@gmail.com>
2023-05-28 14:31:18 +0300

    order summary

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,7 +1,31 @@
+"use client";
+import CartItem from "@/components/Cart/CartItem";
+import OrderSummary from "@/components/Cart/OrderSummary";
+import { useAppSelector } from "@/hooks";
 import React from "react";
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const { products } = useAppSelector((state) => state.cart);
+  return (
+    <div className="p-4 container mx-auto py-20 px-8">
+      <span className="font-bold text-3xl w-full">Checkout</span>
+      <hr className="mt-8 mb-3" />
+      <div className="w-full flex relative">
+        <div className="w-[60%] border-r pr-8">Shipping Info</div>
+        <div className="w-[40%] sticky top-0 ">
+          <div className="px-4 flex flex-col">
+            {products?.map((product: any, index: number) => (
+              <CartItem key={index} product={product} isPreview={false} />
+            ))}
+          </div>
+
+          <div className="w-full sticky top-40">
+            <OrderSummary />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
