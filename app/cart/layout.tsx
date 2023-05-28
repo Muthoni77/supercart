c05a0c4cc43edfd8b33a87fdaaed401a18b615f5
jonathan_baraza <jbaraza46@gmail.com>
2023-05-28 14:21:04 +0300

    import order

--- app/cart/layout.tsx
+++ app/cart/layout.tsx
@@ -1,6 +1,6 @@
+import React from "react";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import React from "react";
 
 const CartLayout = ({ children }: { children: React.ReactNode }) => {
   return (
