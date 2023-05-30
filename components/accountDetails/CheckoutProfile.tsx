a086ef3b657e96c12ba8c4be21cfb79f982c09f0
jonathan_baraza <jbaraza46@gmail.com>
2023-05-30 12:56:34 +0300

    feat: changeable card

--- components/accountDetails/CheckoutProfile.tsx
+++ components/accountDetails/CheckoutProfile.tsx
@@ -13,29 +13,32 @@ const CheckoutProfile = () => {
       <div className="w-full flex flex-col space-y-7">
         {/* Contact Info */}
         <CheckoutProfileCard
-          icon={<HiOutlineUserCircle size={30} className="text-gray-700" />}
+          icon={<HiOutlineUserCircle size={30} className="text-gray-800" />}
           title="CONTACT INFO"
           details={{ one: "Jonathan Baraza", two: "254704783187" }}
+          changeable={false}
         />
 
         {/* Shippping Address */}
         <CheckoutProfileCard
-          icon={<MdOutlineLocalShipping size={30} className="text-gray-700" />}
+          icon={<MdOutlineLocalShipping size={30} className="text-gray-800" />}
           title="SHIPPING ADDRESS"
           details={{
             one: "St. Paul's Road, Norris, SD 57560, Dakota, USA",
             two: "",
           }}
+          changeable={false}
         />
 
         {/* Payment method*/}
         <CheckoutProfileCard
-          icon={<MdOutlineCreditCard size={30} className="text-gray-700" />}
+          icon={<MdOutlineCreditCard size={30} className="text-gray-800" />}
           title="PAYMENT METHOD"
           details={{
             one: "GOOGLE/APP WALET",
             two: "XXX-XXX-XXX-187",
           }}
+          changeable={true}
         />
       </div>
     </>
