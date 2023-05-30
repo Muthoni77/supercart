ef894d7768ae3a16899d89f3295406037a40c84f
jonathan_baraza <jbaraza46@gmail.com>
2023-05-30 13:43:57 +0300

    padding and margins fix

--- components/accountDetails/SelectPaymentMethod.tsx
+++ components/accountDetails/SelectPaymentMethod.tsx
@@ -3,27 +3,7 @@ import { BsCreditCard } from "react-icons/bs";
 
 const SelectPaymentMethod = () => {
   return (
-    <div className=" border p-3 flex flex-col space-y-6 pt-6">
-      <div>
-        <label
-          htmlFor="card"
-          className="flex items-center space-x-5 cursor-pointer"
-        >
-          <input
-            id="card"
-            type="radio"
-            name="payment_method"
-            value={"card"}
-            style={{ width: "22px", height: "22px" }}
-          />
-          <div className="flex space-x-6 items-center">
-            <div className="border border-gray-600 border-2  rounded-xl p-2">
-              <BsCreditCard size={30} />
-            </div>
-            <span> Debit / Credit Card</span>
-          </div>
-        </label>
-      </div>
+    <div className=" border-b border-l border-r p-3 flex flex-col space-y-6 p-6 rounded-b-xl">
       <div>
         <label
           htmlFor="mpesa"
@@ -34,7 +14,7 @@ const SelectPaymentMethod = () => {
             type="radio"
             name="payment_method"
             value={"mpesa"}
-            style={{ width: "22px", height: "22px" }}
+            style={{ width: "28px", height: "28px" }}
           />
 
           <div className="flex space-x-6 items-center">
@@ -45,6 +25,26 @@ const SelectPaymentMethod = () => {
               />
             </div>
             <span> Mpesa</span>
+          </div>
+        </label>
+      </div>
+      <div>
+        <label
+          htmlFor="card"
+          className="flex items-center space-x-5 cursor-pointer"
+        >
+          <input
+            id="card"
+            type="radio"
+            name="payment_method"
+            value={"card"}
+            style={{ width: "28px", height: "28px" }}
+          />
+          <div className="flex space-x-6 items-center">
+            <div className="border border-gray-600 border-2  rounded-xl p-2">
+              <BsCreditCard size={30} />
+            </div>
+            <span> Debit / Credit Card</span>
           </div>
         </label>
       </div>
