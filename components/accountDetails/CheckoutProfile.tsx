53125f124f54c46da4eb5e71491bd8c64eaa7eca
jonathan_baraza <jbaraza46@gmail.com>
2023-05-30 10:25:51 +0300

    icons props

--- components/accountDetails/CheckoutProfile.tsx
+++ components/accountDetails/CheckoutProfile.tsx
@@ -13,14 +13,14 @@ const CheckoutProfile = () => {
       <div className="w-full flex flex-col space-y-7">
         {/* Contact Info */}
         <CheckoutProfileCard
-          icon={<HiOutlineUserCircle size={30} />}
+          icon={<HiOutlineUserCircle size={30} className="text-gray-700" />}
           title="CONTACT INFO"
           details={{ one: "Jonathan Baraza", two: "254704783187" }}
         />
 
         {/* Shippping Address */}
         <CheckoutProfileCard
-          icon={<MdOutlineLocalShipping size={30} />}
+          icon={<MdOutlineLocalShipping size={30} className="text-gray-700" />}
           title="SHIPPING ADDRESS"
           details={{
             one: "St. Paul's Road, Norris, SD 57560, Dakota, USA",
@@ -30,7 +30,7 @@ const CheckoutProfile = () => {
 
         {/* Payment method*/}
         <CheckoutProfileCard
-          icon={<MdOutlineCreditCard size={30} />}
+          icon={<MdOutlineCreditCard size={30} className="text-gray-700" />}
           title="PAYMENT METHOD"
           details={{
             one: "GOOGLE/APP WALET",
