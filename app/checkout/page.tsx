6ddc2d6cf52a6b5b211764887ea519966a4c612a
jonathan_baraza <jbaraza46@gmail.com>
2023-06-11 14:44:08 +0300

    test custom emit

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -45,10 +45,12 @@ const Checkout = () => {
     dispatch(setCheckoutRequestID(response?.data?.CheckoutRequestID));
   };
 
+  const handleMpesaCallback = (callbackData: any) => {
+    console.log("This is the callback data from server");
+    console.log(callbackData);
+  };
   //socket instances
   useEffect(() => {
-    console.log("socket");
-    console.log(socket);
     function onConnect() {
       setIsConnected(true);
     }
@@ -59,18 +61,17 @@ const Checkout = () => {
 
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
+    socket.on("mpesaCallback", handleMpesaCallback);
 
     return () => {
       socket.off("connect", onConnect);
       socket.off("disconnect", onDisconnect);
+      socket.off("mpesaCallback", handleMpesaCallback);
     };
   }, []);
   return (
     <>
       <div className="p-4 container mx-auto py-20 px-8">
-        <span className="font-bold text-3xl w-full">
-          Socket {String(isConnected)}
-        </span>
         <span className="font-bold text-3xl w-full">Checkout</span>
         <hr className="mt-8 mb-6" />
         <div className="w-full flex relative">
@@ -78,7 +79,12 @@ const Checkout = () => {
             <CheckoutProfile />
           </div>
           <div className="w-[40%] sticky top-0 ">
-            <span className="font-bold text-base mb-3 px-4">Order Summary</span>
+            <span
+              className="font-bold text-base mb-3 px-4"
+              onClick={() => socket.emit("hello", { author: user.username })}
+            >
+              Order Summary
+            </span>
             <div
               className="w-full  px-4 flex flex-col max-h-[50vh] no-scrollbarss"
               style={{ overflowY: "auto" }}
