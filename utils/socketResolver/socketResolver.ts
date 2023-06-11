775b11ac3707fdd73abadca9e9f25278a37be396
jonathan_baraza <jbaraza46@gmail.com>
2023-06-11 15:19:10 +0300

    fix: wss error

--- utils/socketResolver/socketResolver.ts
+++ utils/socketResolver/socketResolver.ts
@@ -1,10 +1,17 @@
 import { io } from "socket.io-client";
 
 const URL = process.env.BACKEND_URL!;
-export const socket = io(URL, {
-  transports: ["websocket"],
-});
+// export const socket = io(URL, {
+//   transports: ["websocket"],
+// });
 
+export const wssResolver = () => {
+  const connectUrl = `ws://localhost:4000`;
+  const socket = io(connectUrl, {
+    transports: ["websocket"],
+  });
+  return socket;
+};
 // export const wssResolver = (namespace) => {
 //   const connectUrl = `wss://${
 //     initialState.backendUrl.split("//")[1]
